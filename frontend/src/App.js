abc50598ff45d5eed632dc323c605d742835db6f
venkata-naveen-varma <venkata2naveen@gmail.com>
2025-01-01 10:50:44 -0500

    app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,12 +6,28 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import HomePage from './pages/HomePage';
 import { AuthProvider, useAuth } from './context/AuthContext';
+import Homepage from './pages/HomePage';
 
-
+const ProtectedRoute = ({ children }) => {
+  const { auth } = useAuth();
+  return auth.user ? children : <Navigate to='/login'/>
 
 const App = () => {
   return (
-    <div>App</div>
+    <AuthProvider>
+      <Router>
+        <Navbar/>
+          <Routes>
+            <Route path='/login' element={<Login/>} />
+            <Route path='/signup' element={<Signup/>} />
+            <Route path='/' element={
+              <ProtectedRoute>
+                <Homepage/>
+              </ProtectedRoute>
+            }/>
+          </Routes>
+      </Router>
+    </AuthProvider>
   )
 }
 
